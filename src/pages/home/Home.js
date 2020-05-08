c61bf3764c5c0ea85685384c6f672b321d1e0df6
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-05-08 11:55:47 +0800

    [XingfengYang] #Feat add features

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import './Home.css';
 import Container from "react-bootstrap/Container";
 import TestSplash from "../../components/text_splash/TestSplash";
+import Features from "../../components/features/Features";
 
 class Home extends Component {
     render() {
         return (<Container className="Home">
 
             <TestSplash/>
+            <Features/>
 
         </Container>);
     };
