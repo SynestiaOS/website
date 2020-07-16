a0089002036ec0c290d34eec2b7758bbe7d0ba6f
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-16 13:48:10 +0800

    [XingfengYang] #N/A

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,7 +10,7 @@ function Header() {
     return (
         <Container className="Header">
             <Row style={{padding: 0, paddingTop: '1em'}}>
-                <Col style={{padding: 0, height: '4em'}}>
+                <Col style={{padding: 0, margin: 0}}>
                     <Navbar bg="#fff" variant="light" style={{textAlign: 'left', padding: 0}}>
                         <Nav style={{width: '100%'}}>
                             <Nav.Link href="#home" style={{color: '#fff', background: '#000', fontWeight: 'bold'}}>ZionLab</Nav.Link>
@@ -18,9 +18,10 @@ function Header() {
                                       style={{color: '#fff', background: '#000', fontWeight: 'normal', marginLeft: '0.2em'}}>SynestiaOS</Nav.Link>
                             <Nav.Link href="https://github.com/SynestiaOS"
                                       style={{color: '#000', fontWeight: 'bold', right: 0, position: 'absolute'}}>Blog</Nav.Link>
-                            <Nav.Link href="https://github.com/SynestiaOS" style={{color: '#000', fontWeight: 'bold',right: '3em', position: 'absolute'}}>Docs</Nav.Link>
+                            <Nav.Link href="https://github.com/SynestiaOS"
+                                      style={{color: '#000', fontWeight: 'bold', right: '3em', position: 'absolute'}}>Docs</Nav.Link>
                         </Nav>
-                    </Navbar>x
+                    </Navbar>
                 </Col>
             </Row>
         </Container>
