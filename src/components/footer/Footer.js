d14b9cfa17f45216fa09a7ececc73a9f03098842
NeoYang <nerosoft@outlook.com>
2020-09-30 22:21:03 +0800

    x

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -32,9 +32,9 @@ function Footer() {
                     <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
                        href={"http://kerneltravel.net"}>Linux Kernel Travel</a>
                     <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
-                       href={"https://templeos.org/"}>RIP Terry A. Davis</a>
+                       href={"http://www.book-os.org/"}>BookOS</a>
                     <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
-                       href={"https://github.com/SynestiaOS"}>Github</a>
+                       href={"https://templeos.org/"}>RIP Terry A. Davis</a>
                 </Col>
             </Row>
         </Container>
