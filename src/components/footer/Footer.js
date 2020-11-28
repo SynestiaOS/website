8a7bc557a0334d933598502a79b9e72d798fcce9
NeoYang <nerosoft@outlook.com>
2020-11-28 22:00:14 +0800

    [XingfengYang] #N/A

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,41 +2,48 @@ import React from 'react';
 import './Footer.css';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import {Col} from "react-bootstrap";
+import {Col, Image} from "react-bootstrap";
 
 function Footer() {
     return (
         <Container className="Footer">
             <Row>
-                <Col md={3}>
-                    <h5 style={{marginBottom:'0.5em'}}>AboutUs</h5>
-                    <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
-                       href={"https://github.com/SynestiaOS"}>About</a>
-                    <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}} href={"https://github.com/SynestiaOS"}>Contact
-                        Us</a>
+                <Col md={3} style={{paddingLeft: '3em', paddingRight: '8em'}}>
+                    <Image src={'./imgs/WeiJi.png'}/>
                 </Col>
-                <Col md={3}>
-                    <h5 style={{marginBottom:'0.5em'}}>Develop</h5>
-                    <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
-                       href={"/docs"}>Docs</a>
-                    <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
-                       href={"/blog"}>News</a>
-                    <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
-                       href={"/faq"}>FAQ</a>
-                </Col>
-                <Col md={3}>
-                    <h5 style={{marginBottom:'0.5em'}}>Support</h5>
-                    <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
-                       href={"/sponsor"}>Sponsor</a>
-                    <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
-                       href={"https://patreon.com/SynestiaOS"}>Donate</a>
-                </Col>
-                <Col md={3}>
-                    <h5 style={{marginBottom:'0.5em'}}>Additional Links</h5>
-                    <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
-                       href={"http://kerneltravel.net"}>Linux Kernel Travel</a>
-                    <a style={{ color:'#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
-                       href={"https://templeos.org/"}>RIP Terry A. Davis</a>
+                <Col md={9} style={{paddingLeft: '3em', paddingRight: '8em'}}>
+                    <Row style={{padding:0}}>
+                        <Col md={3}>
+                            <h5 style={{marginBottom: '0.5em'}}>About</h5>
+                            <a style={{color: '#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
+                               href={"/home"}>About us</a>
+                            <a style={{color: '#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
+                               href={"/home"}>Contact us</a>
+                        </Col>
+                        <Col md={3}>
+                            <h5 style={{marginBottom: '0.5em'}}>Develop</h5>
+                            <a style={{color: '#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
+                               href={"/docs"}>Docs</a>
+                            <a style={{color: '#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
+                               href={"/blog"}>News</a>
+                            <a style={{color: '#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
+                               href={"/faq"}>FAQ</a>
+                        </Col>
+                        <Col md={3}>
+                            <h5 style={{marginBottom: '0.5em'}}>Support</h5>
+                            <a style={{color: '#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
+                               href={"/sponsor"}>Sponsor</a>
+                            <a style={{color: '#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
+                               href={"https://patreon.com/SynestiaOS"}>Donate</a>
+                        </Col>
+                        <Col md={3}>
+                            <h5 style={{marginBottom: '0.5em'}}>Additional Links</h5>
+                            <a style={{color: '#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
+                               href={"http://kerneltravel.net"}>Linux Kernel Travel</a>
+                            <a style={{color: '#888', fontWeight: 'lighter', margin: 0, padding: 0, display: 'block'}}
+                               href={"https://templeos.org/"}>RIP Terry A. Davis</a>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
